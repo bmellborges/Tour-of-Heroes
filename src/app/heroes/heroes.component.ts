ab7a2371e0b5276a0bcc5fd89847031a97c12c51
bmellborges <brunomelloborges@outlook.com>
2021-03-16 20:50:24 -0300

    3º desafio completo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,13 +16,15 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   heroes = HEROES;
-
+  
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+    this.selectedHero = hero;
+  }
   
-  constructor() { }
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
